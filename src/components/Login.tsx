aa499a394616051d92d1a5b30eee49495afb1e52
prime-infinity <soe.a.prime@gmail.com>
2023-03-17 02:18:08 -0700

    done login page,added logos and made minor ui adjustments perfection

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,9 +4,15 @@ function Login() {
   return (
     <div className="container-fluid">
         <div className="row justify-content-center d-flex align-items-center" style={{height:"100vh"}}>
+            <div className="logo-float" >
+                
+                <img src="/images/general/union.png" className="logo-float_sqr" alt="unionlogo" />
+                <img src="/images/general/lendsqr.png" className="logo-float_wri" alt="lendsqrlogo" />
+                
+            </div>
             <div className="col-md-6 col-12 d-none d-md-block">
                 <div className="row justify-content-center">
-                    <div className="col-10 pt-5 mt-5">
+                    <div className="col-10 pt-5 mt-3">
                         <img src="/images/loginpage/pablo-sign-in1.svg" className="img-fluid" alt="loginpagelogo" />
                     </div>
                 </div>
@@ -15,12 +21,14 @@ function Login() {
                 <div className="row d-flex" style={{width:"-webkit-fill-available"}}>
                     <div className="col-md-9 offset-md-1 ps-5 pe-5">
                         <div className="form-main">
-                            <div className="form-main_text pb-5">
-                                <div className="">
-                                    <h3 className="fw-7 fs-40">Welcome!</h3>
+                            <div className="form-main_text pb-5 mb-3">
+                                <div className="pb-2">
+                                    <h3 className="fw-7 fs-40 d-none d-md-block">Welcome!</h3>
+                                    <h3 className="fw-7 text-center fs-40 d-block d-md-none">Welcome!</h3>
                                 </div>
                                 <div>
-                                    <span className="fs-20">Enter details to login</span>
+                                    <span className="fs-20 d-none d-md-block">Enter details to login</span>
+                                    <span className="fs-20 text-center d-block d-md-none">Enter details to login</span>
                                 </div>
                             </div>
                             
@@ -28,9 +36,12 @@ function Login() {
                                 <div className="mb-4">
                                     <input type="email" placeholder="Email" className="form-control input-main-type" />
                                 </div>
-                                <div>
-                                    <input type="password" placeholder="Password" className="form-control input-main-type" />
+                                
+                                <div className="input-group">
+                                    <input type="password" placeholder="Password" className="form-control input-main-type" style={{borderRight:"0"}}/>
+                                    <span className="input-group-text fs-12 fw-6">SHOW</span>
                                 </div>
+
                                 <div className="pt-3 pb-4">
                                     <span className="color-primary fs-12 fw-6">FORGOT PASSWORD?</span>
                                 </div>
